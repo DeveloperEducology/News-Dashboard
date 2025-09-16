9ca19c63ca1a592cb14abe0db26ac14695859f1d
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-09-16 20:58:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ArticlesTable from "./news/ArticlesTable";
 import NewDashboard from "./news/NewDashboard";
 import EmbedSocialPage from "./news/EmbedPage";
 import TwitterNodeList from "./news/TwitterNodeList";
+import FetchTweet from "./news/FetchTweet";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Route path="/news-dashboard" element={<NewDashboard />} />
       <Route path="/embed" element={<EmbedSocialPage />} />
       <Route path="/twi" element={<TwitterNodeList />} />
-            
+  <Route path="/fetch-tweet" element={<FetchTweet />} />
       <Route path="/news/list" element={<ListNews />} />
       <Route path="/ArticlesTable" element={<ArticlesTable />} />
 
